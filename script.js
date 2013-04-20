32fe219987a32128e7ee74b3c84bf529c4bc9851
Martin Forsgren <martin.forsgren@gmail.com>
2013-04-20 12:36:41 +0200

    Merge specials and dictionary, pass around dictionary just like stack.

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 var stack = [];
-var dict = {
-  ":": function(s) { dict[s.pop()] = s.pop(); }, // uses dict
-  "+": function(s) { s.push(s.pop() + s.pop()) },
-  "dup": function(s) { var v = s.pop(); s.push(v); s.push(v) }
+var dictionary = {
+  "+": function(s) { s.push(s.pop() + s.pop()); },
+  "*": function(s) { s.push(s.pop() * s.pop()); },
+  "dup": function(s) { var v = s.pop(); s.push(v); s.push(v); },
   "?": function(s) {
     var f = s.pop(), t = s.pop(), test = s.pop();
-    return test ? t : f;
+    s.push(test ? t : f);
   },
-  "call": function(s) { s.pop()(s); }
-};
-var specials = {
-  "[": function(stack, tokens, idx) {
+  // specials
+  "call": function(s, dict) { s.pop()(s, dict); },
+  ":": function(s, dict) { dict[s.pop()] = s.pop(); },
+  "[": function(stack, dict, tokens, idx) {
     var end = matchingIndex("[", "]", tokens, idx);
     stack.push(makeFun(tokens.slice(idx+1,end)));
     return end;
   },
-  "'": function(stack, tokens, idx) {
+  "'": function(stack, _, tokens, idx) {
     stack.push(tokens[idx+1]);
     return idx+1;
   }
@@ -32,29 +32,30 @@ function matchingIndex(left, right, tokens, startIdx) {
 }
 
 function makeFun(tokens) {
-  console.log("fun created:", tokens);
-  return function(stack) { interpret(stack, tokens); };
+  // console.log("fun created:", tokens);
+  return function(stack, dict) { interpret(stack, dict, tokens); };
 }
 
-function parseNum(stack, tokens, idx) {
+function parseNum(stack, _, tokens, idx) {
   stack.push(parseFloat(tokens[idx]));
   return idx;
 }
 
-function interpret(stack, tokens) {
+function interpret(stack, dict, tokens) {
   for(var i = 0; i < tokens.length; i++) {
     var token = tokens[i];
-    if(dict[token])
-      dict[token](stack);
-    else
-      i = (specials[token] || parseNum)(stack, tokens, i);
-
-    console.log("one interp: ", stack, tokens, i);
+    var ret = (dict[token] || parseNum)(stack, dict, tokens, i);
+    if(!isNaN(ret))
+      i = ret;
+//    console.log("one interp: ", "" + stack, tokens, i);
   }
   return stack;
 }
 
-function i(str){ interpret(stack, str.split(" ")); return stack; }
+function i(stack, dict, str){
+  interpret(stack, dict, str.trim().split(/\s+/g));
+  return stack;
+}
 
 //Drawing
 var canvas = document.getElementById("canvas");
